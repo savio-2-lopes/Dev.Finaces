7e5b85bc578cbfc592c59cc79f0cd7c9b5f0da7f
Savio Lopes <60948849+savio-2-lopes@users.noreply.github.com>
2021-02-15 16:59:36 -0300

    Update lesson four and fix bug

--- scripts.js
+++ scripts.js
@@ -160,9 +160,8 @@ const Form = {
 
 const Utils = {
     formatAmount(value) {
-        //value = Number(value) * 100
-        value = Number(value.replace(/\,\./g, "")) * 100
-        return value
+        value = value * 100
+        return Math.round(value)
     },
 
     formatDate(date) {
